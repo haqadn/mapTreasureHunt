9fd938f43ee9295fc0956e4f9c3cd7775b968c5b
Mohaimenul Adnan <adnan007.id@gmail.com>
2016-04-02 20:53:58 +0600

    Improved asset links and plugin loading.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,17 +3,19 @@ jQuery(document).ready(function($){
 	/* =======================================================
 		Initiate countdown timer
 	   ======================================================= */
-	$('.countdown').countdown({
-		end_time: "2017/06/21 14:27:28 +0600",
-		show_day: false,
-		wrapper: function( unit ){
-			var wrapper = $('<div class="' + unit.toLowerCase() + '_wrapper col-md-4" />');
-			wrapper.append('<span class="counter" />');
-			wrapper.append('<span class="title">' + unit + '</span>');
+	if(typeof $.fn.countdown !== 'undefined'){
+		$('.countdown').countdown({
+			end_time: "2017/06/21 14:27:28 +0600",
+			show_day: false,
+			wrapper: function( unit ){
+				var wrapper = $('<div class="' + unit.toLowerCase() + '_wrapper col-md-4" />');
+				wrapper.append('<span class="counter" />');
+				wrapper.append('<span class="title">' + unit + '</span>');
 
-			return wrapper;
-		}
-	});
+				return wrapper;
+			}
+		});
+	}
 });
 
 
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,17 +4,17 @@
         <title>@yield( 'title' ) - {{ config('app.title') }}</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link href="asset/css/app.css" rel="stylesheet" type="text/css">
+        <link href="{{ asset('asset/css/app.css') }}" rel="stylesheet" type="text/css">
         
         @section('scripts')
         <!-- jQuery -->
-        <script src="asset/js/jquery.min.js"></script>
+        <script src="{{ asset('asset/js/jquery.min.js') }}"></script>
 
         <!-- Bootstrap JS -->
-        <script src="asset/js/bootstrap.min.js"></script>
+        <script src="{{ asset('asset/js/bootstrap.min.js') }}"></script>
 
         <!-- Main JS -->
-        <script src="asset/js/app.js"></script>
+        <script src="{{ asset('asset/js/app.js') }}"></script>
         @show
 
     </head>
@@ -29,7 +29,7 @@
                         <span class="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" href="{{ config('app.url') }}">
-                        <img src="asset/images/th-logo.png" alt="{{ config('app.title') }}" class="logo">
+                        <img src="{{ asset('asset/images/th-logo.png') }}" alt="{{ config('app.title') }}" class="logo">
                     </a>
                 </div>
                 <div class="collapse navbar-collapse" id="primary-navigation">
@@ -42,6 +42,7 @@
                     @if(Auth::check())
                         <ul class="nav navbar-nav navbar-right">
                             <li><a href="{{ url('/logout') }}">{{ trans('auth.logout') }}</a></li>
+                        </ul>
                     @else
                         @include('auth-action')
                     @endif
