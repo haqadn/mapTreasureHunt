a14f5f33f7aa6a655ddd92b859eb3f4a9a465610
Mohaimenul Adnan <adnan007.id@gmail.com>
2016-04-03 13:05:18 +0600

    Added google map andremoved old google map library.

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,12 +1,16 @@
 @extends('template')
 
-@section('title', trans('pages.home'))
+@section('title', trans('pages.game'))
 
 @section('scripts')
 	@parent
 
+	<!-- Google MAPS API -->
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_API_KEY') }}&libraries=places"
+  type="text/javascript"></script>
+
 @endsection
 
 @section('content')
-
+	<div id="map"></div>
 @endsection
