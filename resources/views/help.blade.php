7ed9bb4f7403c4e97c42e78ec3e842dcef00d2c8
Mohaimenul Adnan <adnan007.id@gmail.com>
2016-04-06 12:22:59 +0600

    Instructions updated.

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -8,19 +8,26 @@
 		<article>
 			<h1>{{ trans('pages.help') }}</h1>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi laborum, cupiditate nostrum tempora modi obcaecati repudiandae. Ullam illum quibusdam reiciendis, odio eveniet possimus aliquid vel nostrum dolorum corporis facilis, incidunt.
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, quo reprehenderit excepturi optio nulla vitae illo harum fugiat possimus, quis impedit suscipit hic numquam consequuntur, amet voluptatem id cum error.
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam accusamus voluptates, quos veniam delectus laboriosam praesentium magni est unde quibusdam molestiae, fuga doloremque beatae maiores. Veniam quia, tenetur accusantium dignissimos!
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque consectetur laborum temporibus, laboriosam error non. Aut nam, id illum eius natus ullam fugit quisquam, recusandae enim est, cum ipsam perspiciatis!
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi laborum, cupiditate nostrum tempora modi obcaecati repudiandae. Ullam illum quibusdam reiciendis, odio eveniet possimus aliquid vel nostrum dolorum corporis facilis, incidunt.
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, quo reprehenderit excepturi optio nulla vitae illo harum fugiat possimus, quis impedit suscipit hic numquam consequuntur, amet voluptatem id cum error. <br>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam accusamus voluptates, quos veniam delectus laboriosam praesentium magni est unde quibusdam molestiae, fuga doloremque beatae maiores. Veniam quia, tenetur accusantium dignissimos!
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque consectetur laborum temporibus, laboriosam error non. Aut nam, id illum eius natus ullam fugit quisquam, recusandae enim est, cum ipsam perspiciatis! <br>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi laborum, cupiditate nostrum tempora modi obcaecati repudiandae. Ullam illum quibusdam reiciendis, odio eveniet possimus aliquid vel nostrum dolorum corporis facilis, incidunt.
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, quo reprehenderit excepturi optio nulla vitae illo harum fugiat possimus, quis impedit suscipit hic numquam consequuntur, amet voluptatem id cum error.
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam accusamus voluptates, quos veniam delectus laboriosam praesentium magni est unde quibusdam molestiae, fuga doloremque beatae maiores. Veniam quia, tenetur accusantium dignissimos!
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque consectetur laborum temporibus, laboriosam error non. Aut nam, id illum eius natus ullam fugit quisquam, recusandae enim est, cum ipsam perspiciatis!
+				Please follow the instructions below instruction to play LUCC Tech Storm Online Treasure Hunt!
 			</p>
+			<ol>
+				<li>Keep in mind that LUCC rocks B|.</li>
+				<li>Your clock starts ticking instantly when the contest begins.</li>
+				<li>Start by going to the "Game" page from the navigation bar.</li>
+				<li>You will be given a clue when you first arrive at the page.</li>
+				<li>Use this clue to find out the location of the next clue in the map.</li>
+				<li>Use the search feature of the map to find clue location faster.</li>
+				<li>Different clues require different minimum zoom level to be spotted. If you don't see your clue on your expected spot, look closer.</li>
+				<li>The team which finds the treaser before everyone else, wins.</li>
+				<li>If at least three teams are not able to find the treasure, winners will be decided by number of clues discovered.</li>
+				<li>When two or more teams have same number of clues discovered, time taken to discover the clues will be taken into account.</li>
+				<li>If you don't understand the ranking rules, just keep an eye on the "Leaderboard".</li>
+				<li>You can see all of your opponents progress in the leaderboard.</li>
+				<li>The questions and clues are given in Bengali, you must provide answer in English.</li>
+				<li>You are free to look into web or call someone for answers that you may face during the treasure hunting journey.</li>
+				<li>Any attempt to hack the system to manupulate the results will result in disqualification.</li>
+				<li>Leaderboard may be frozen during the end of the contest.</li>
+			</ol>
 		</article>
 	</div>
 </div>
